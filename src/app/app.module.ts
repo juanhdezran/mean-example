b49a38320e9243b58af44eb817645c882199911b
John <juanhdezran@gmail.com>
2018-09-18 23:20:26 -0500

    components added, router and routes implemented and imported, material toolbar imported and implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './components/list/list.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatToolbar } from '@angular/material';
+
+const routes: Routes = [
+  {path: 'create', component: CreateComponent},
+  {path: 'list', component: ListComponent},
+  {path: 'edit/:id', component: EditComponent},
+  {path: '', redirectTo:'list', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CreateComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MatToolbar
   ],
   providers: [],
   bootstrap: [AppComponent]
