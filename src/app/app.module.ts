09dbd92713e10a5f1f67e9038094bf477e4d260a
Juan Hernández <juanhdezran@gmail.com>
2018-11-06 23:23:57 -0600

    list component implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,27 @@ import { ListComponent } from './components/list/list.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
 import { RouterModule, Routes } from '@angular/router';
-import { MatToolbar } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatOptionModule,
+  MatSelectModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatTableModule,
+  MatDividerModule,
+  MatSnackBarModule
+} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { IssueService } from './services/issue.service';
 
 const routes: Routes = [
-  {path: 'create', component: CreateComponent},
-  {path: 'list', component: ListComponent},
-  {path: 'edit/:id', component: EditComponent},
-  {path: '', redirectTo:'list', pathMatch: 'full'}
+  { path: 'create', component: CreateComponent },
+  { path: 'list', component: ListComponent },
+  { path: 'edit/:id', component: EditComponent },
+  { path: '', redirectTo: 'list', pathMatch: 'full' }
 ];
 
 @NgModule({
@@ -27,9 +41,20 @@ const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(routes),
-    MatToolbar
+    HttpClientModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
+    MatDividerModule,
+    MatSnackBarModule
   ],
-  providers: [],
+  providers: [IssueService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
